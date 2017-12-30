a93129f41f50402d6bf1b14724eeab2ffdd4c540
Derek Santos <derek@santosapps.com>
2017-12-30 00:24:57 -0500

    docs(license): Update header in all source files to pointer to LICENSE file.

--- entityquery.go
+++ entityquery.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2017 Derek Santos. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be found
+// in the LICENSE file.
 package entigo
 
 import (
